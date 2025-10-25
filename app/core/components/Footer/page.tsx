3c9bc681bf8ee8fb616b123d3c56eff42d264add
Felix von dem Berge <felixvdb19@gmail.com>
2025-10-25 07:53:51 +0200

    style: general styling for header and footer

--- app/core/components/Footer/page.tsx
+++ app/core/components/Footer/page.tsx
@@ -1,3 +1,3 @@
 export default function Footer() {
-    return <footer>Footer</footer>;
+    return <footer className="p-8 bg-gray-900">Footer</footer>;
 }
--- app/core/components/Header/page.tsx
+++ app/core/components/Header/page.tsx
@@ -1,3 +1,3 @@
 export default function Header() {
-    return <header>Header</header>;
+    return <header className="p-8 bg-gray-900">Header</header>;
 }
